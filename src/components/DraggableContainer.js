cd4ed200184d44069842ea7d673e4009d4a376e4
Jimmy Oliger <jimmyoliger@gmail.com>
2020-04-09 21:05:52 +0200

    Improve DraggableContainer
    DraggableContainer was adding new event listeners on every renders.
    My computer was frying because of this.

--- src/components/DraggableContainer.js
+++ src/components/DraggableContainer.js
@@ -4,14 +4,44 @@ import * as PIXI from 'pixi.js';
 export default CustomPIXIComponent(
   {
     customDisplayObject: (props) => new PIXI.Container(),
-    customApplyProps: function (instance, oldProps, newProps) {
+    customWillDetach(instance) {
+      instance.off('pointerdown', instance._handleDragStart);
+    },
+    customApplyProps(instance, oldProps, newProps) {
       instance.interactive = true;
-      instance.buttonMode = true;
-      instance
-        .on('pointerup', newProps.onDragEnd)
-        .on('pointerdown', newProps.onDragStart)
-        .on('pointerupoutside', newProps.onDragEnd)
-        .on('pointermove', newProps.onDragMove);
+
+      instance.cursor = 'grab';
+
+      const handleDragStart = (event) => {
+        newProps.onDragStart(event);
+
+        instance.cursor = 'grabbing';
+
+        instance.on('pointermove', handleDragMove);
+        instance.on('pointerup', handleDragEnd);
+      };
+
+      const handleDragMove = (event) => {
+        newProps.onDragMove(event);
+
+        instance.cursor = 'grabbing';
+      };
+
+      const handleDragEnd = (event) => {
+        newProps.onDragEnd(event);
+
+        instance.cursor = 'grab';
+
+        instance.off('pointermove', handleDragMove);
+        instance.off('pointerup', handleDragEnd);
+      };
+
+      if (instance._handleDragStart) {
+        instance.off('pointerdown', instance._handleDragStart);
+      }
+
+      instance._handleDragStart = handleDragStart;
+      instance.on('pointerdown', instance._handleDragStart);
     },
   },
   'DraggableContainer'
